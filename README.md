0596a4a0694b94b661a0a784cbbd411522432bb6
Lemi <leminegeso@gmail.com>
2025-04-07 14:18:05 +0300

    begin working on simple calculator

---
+++ README.md
@@ -0,0 +1,2 @@
+**Python Projects**
+Here I am going to show you my projects using python.
---
+++ simple-calculator.py
@@ -0,0 +1,16 @@
+from tkinter import Tk, Entry, Button, StringVar
+
+class Calculator:
+    def _init_(self,master):
+        master.title("Calculator")
+        master.geometry('357x420+0+0')
+        master.config(bg.'gray')
+        master.resizable(False,False)
+
+        self.equation.StringVar()
+        self.entry_value=''
+        Entry(width=17,bg='#fff',font=('Arial Bold',20),textvariable=self.equation).place(x=0,y=0)
+
+        Button(wudth=11,  height
+root = Tk()
+root.mainloop()
