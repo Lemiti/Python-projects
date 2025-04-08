845658a84983299d65f1ff96b85b152af97c4533
Lemi <leminegeso@gmail.com>
2025-04-08 23:19:59 +0300

    fixe some code?

--- simple-calculator.py
+++ simple-calculator.py
@@ -4,13 +4,19 @@ class Calculator:
     def _init_(self,master):
         master.title("Calculator")
         master.geometry('357x420+0+0')
-        master.config(bg.'gray')
+        master.config(bg='gray')
         master.resizable(False,False)
 
         self.equation.StringVar()
         self.entry_value=''
-        Entry(width=17,bg='#fff',font=('Arial Bold',20),textvariable=self.equation).place(x=0,y=0)
+        Entry(width=17,bg='#fff',font=('Arial Bold',28),textvariable=self.equation).place(x=0,y=0)
 
-        Button(wudth=11,  height
+    def show(self, value):
+        self.entry_value+=str(value)
+        self.equation.set(self.entry_value)
+
+    def clear(self):
+        self.entry_value=''
+        self.equation.set(self, entry_val)
 root = Tk()
 root.mainloop()
